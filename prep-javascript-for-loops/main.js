212dff91d133890f39845db78b837f272c7969d8
Lisa Le <lisale841@gmail.com>
2021-11-18 13:49:11 -0800

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let a = 0; a < 10; a++) {
+  console.log(a * 2);
+}
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+
+}
